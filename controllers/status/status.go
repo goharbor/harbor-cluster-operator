b0fe3a33697824c8a97b8ac1fa4b7551ca74758f
Ghostbaby <zhuhuijunzhj@gmail.com>
2020-04-23 22:30:35 +0800

    add crstatus definition
    Signed-off-by: Ghostbaby <zhuhuijunzhj@gmail.com>

--- controllers/status/status.go
+++ controllers/status/status.go
@@ -1,1 +1,30 @@
 package status
+
+type CRStatus struct {
+	Phase      Phase       `json:"phase"`
+	Properties []*Property `json:"properties"`
+}
+
+type Phase string
+
+const (
+	PendingPhase    Phase = "Pending"
+	DeployingPhase  Phase = "Deploying"
+	ReadyPhase      Phase = "Ready"
+	UpgradingPhase  Phase = "Upgrading"
+	DestroyingPhase Phase = "Destroying"
+)
+
+const (
+	ProperConn  = "Connection"
+	ProperPort  = "Port"
+	ProperUser  = "Username"
+	ProperPass  = "Password"
+	ProperNodes = "AvailableNodes"
+)
+
+type Property struct {
+	Name  string
+	Type  string
+	Value interface{}
+}
