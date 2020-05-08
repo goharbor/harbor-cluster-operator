317818500d87f5e3a424d7a09e18e8bde4b891a1
xiaoyang zhu <zhuxiaoyang1996@gmail.com>
2020-05-08 21:52:30 +0800

    status
    Signed-off-by: xiaoyang zhu <zhuxiaoyang1996@gmail.com>

--- controllers/storage/minio.go
+++ controllers/storage/minio.go
@@ -1,11 +1,13 @@
 package storage
 
 import (
+	"context"
 	"github.com/go-logr/logr"
 	goharborv1 "github.com/goharbor/harbor-cluster-operator/api/v1"
 	"github.com/goharbor/harbor-cluster-operator/lcm"
 	minioClient "github.com/minio/minio-operator/pkg/client/clientset/versioned"
 	miniolisters "github.com/minio/minio-operator/pkg/client/listers/miniooperator.min.io/v1beta1"
+	apisv1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/cache"
 )
@@ -17,12 +19,14 @@ type MinIOReconciler struct {
 	kubeClientSet kubernetes.Interface
 	// minioClientSet is a clientset for our own API group
 	minioClientSet minioClient.Interface
-	// minioInstancesLister lists MinIOInstance from a shared informer's
-	// store.
-	minioInstancesLister miniolisters.MinIOInstanceLister
-	// minioInstancesSynced returns true if the StatefulSet shared informer
-	// has synced at least once.
-	minioInstancesSynced cache.InformerSynced
+	//// minioInstancesLister lists MinIOInstance from a shared informer's
+	//// store.
+	//minioInstancesLister miniolisters.MinIOInstanceLister
+	//// minioInstancesSynced returns true if the StatefulSet shared informer
+	//// has synced at least once.
+	//minioInstancesSynced cache.InformerSynced
+
+	ctx context.Context
 
 	Log logr.Logger
 
@@ -32,7 +36,7 @@ type MinIOReconciler struct {
 
 // Reconciler implements the reconcile logic of minIO service
 func (minio *MinIOReconciler) Reconcile() (*lcm.CRStatus, error) {
-	minio.minioClientSet.MiniooperatorV1beta1().MinIOInstances(minio.Namespace).Get(ctx,)
+	minioinstance,err :=minio.minioClientSet.MiniooperatorV1beta1().MinIOInstances(minio.Namespace).Get(minio.ctx,minio.HarborCluster.Name,apisv1.GetOptions{})
 	return nil, nil
 }
 
--- go.mod
+++ go.mod
@@ -5,7 +5,7 @@ go 1.13
 require (
 	github.com/go-logr/logr v0.1.0
 	github.com/jetstack/cert-manager v0.14.2
-	github.com/minio/minio-operator v1.0.9
+	github.com/minio/minio-operator v0.0.0-20200505050949-8fdab5f7bde2
 	github.com/onsi/ginkgo v1.12.0
 	github.com/onsi/gomega v1.9.0
 	k8s.io/api v0.18.2
