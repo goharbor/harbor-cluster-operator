7e9509b664c7e745c7971df30674d637e0edd529
wangcanfeng <wangcanfeng@corp.netease.com>
2020-05-11 14:12:12 +0800

    add harbor provision implement.
    Signed-off-by: wangcanfeng <wangcanfeng@corp.netease.com>

---
+++ vendor/github.com/goharbor/harbor-operator/api/v1alpha1/metadata.go
@@ -0,0 +1,12 @@
+package v1alpha1
+
+const (
+	HarborClassAnnotation = "goharbor.io/harbor-class"
+)
+
+const (
+	WarningLabel         = "goharbor.io/warning"
+	OperatorNameLabel    = "goharbor.io/name"
+	OperatorVersionLabel = "goharbor.io/version"
+	ComponentNameLabel   = "goharbor.io/component"
+)
