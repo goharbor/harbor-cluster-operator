37d00c5be6c4f506cfee54894acf6b724a3aabe7
xiaoyang zhu <zhuxiaoyang1996@gmail.com>
2020-05-11 23:01:27 +0800

    status
    Signed-off-by: xiaoyang zhu <zhuxiaoyang1996@gmail.com>

--- controllers/storage/provision.go
+++ controllers/storage/provision.go
@@ -9,12 +9,18 @@ import (
 )
 
 const (
-	Kind       = "MinIOInstance"
-	ApiVersion = "miniooperator.min.io/v1beta1"
-	DefaultZone = "zone-harbor"
+	Kind               = "MinIOInstance"
+	ApiVersion         = "miniooperator.min.io/v1beta1"
+	DefaultZone        = "zone-harbor"
+	DefaultCredsSecret = "minio-creds-secret"
+	DefaultMcsSecret   = "minio-mcs-secret"
+	CredsAccesskey     = "bWluaW8="
+	CredsSecretkey     = "bWluaW8xMjM="
 )
 
 func (m *MinIOReconciler) Provision() (*lcm.CRStatus, error) {
+	mcsSecret := m.generateMcsSecret()
+	credsSecret := m.generateCredsSecret()
 
 	panic("implement me")
 }
@@ -43,17 +49,41 @@ func (m *MinIOReconciler) generateMinIOCR() *minio.MinIOInstance {
 			Image: "minio/minio:" + m.HarborCluster.Spec.Stroage.InCluster.Spec.Version,
 			Zones: []minio.Zone{
 				minio.Zone{
-					Name: DefaultZone,
+					Name:    m.HarborCluster.Name+"-"+DefaultZone,
 					Servers: m.HarborCluster.Spec.Stroage.InCluster.Spec.Replicas,
 				},
 			},
-			VolumesPerServer: 1,
-			Mountpath: "/export",
+			VolumesPerServer:    1,
+			Mountpath:           "/export",
 			VolumeClaimTemplate: m.getVolumeClaimTemplate(),
-
+			CredsSecret: &corev1.LocalObjectReference{
+				Name: m.HarborCluster.Name + "-" + DefaultCredsSecret,
+			},
+			PodManagementPolicy: "Parallel",
+			RequestAutoCert: false,
+			CertConfig: &minio.CertificateConfig{
+				CommonName: "",
+				OrganizationName: []string{},
+				DNSNames: []string{},
+			},
+			Env: []corev1.EnvVar{
+				corev1.EnvVar{
+					Name: "MINIO_BROWSER",
+					Value: "on",
+				},
+			},
+			Resources: *m.getResourceRequirements(),//m.HarborCluster.Spec.Stroage.InCluster.Spec.Resources,
+			// TODO
+			//Liveness: ,
+			//Readiness: ,
 
 		},
 	}
+}
+
+func (m *MinIOReconciler) getResourceRequirements() *corev1.ResourceRequirements {
+	// TODO
+	return nil
 }
 
 func (m *MinIOReconciler) getVolumeClaimTemplate() *corev1.PersistentVolumeClaim {
@@ -72,22 +102,48 @@ func (m *MinIOReconciler) generateAnnotations() map[string]string {
 	return nil
 }
 
-func (m *MinIOReconciler) generateHeadlessService() *minio.MinIOInstance {
-	// TODO
-	return nil
-}
-
-func (m *MinIOReconciler) generateMcsSecret() *minio.MinIOInstance {
-	// TODO
-	return nil
-}
-
-func (m *MinIOReconciler) generateCredsSecret() *minio.MinIOInstance {
-	// TODO
-	return nil
-}
-
 func (m *MinIOReconciler) generateService() *minio.MinIOInstance {
 	// TODO
 	return nil
+}
+
+func (m *MinIOReconciler) generateMcsSecret() *corev1.Secret {
+	return &corev1.Secret{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "v1",
+			Kind:       "Secret",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:        m.HarborCluster.Name + "-" + DefaultMcsSecret,
+			Namespace:   m.HarborCluster.Namespace,
+			Labels:      m.getLabels(),
+			Annotations: m.generateAnnotations(),
+		},
+		Type: corev1.SecretTypeOpaque,
+		// TODO
+		Data: map[string][]byte{
+			"mcshmacjwt":         []byte("WU9VUkpXVFNJR05JTkdTRUNSRVQ="),
+			"mcspbkdfpassphrase": []byte("U0VDUkVU"),
+			"mcspbkdfsalt":       []byte("U0VDUkVU"),
+			"mcssecretkey":       []byte("WU9VUk1DU1NFQ1JFVA")},
+	}
+}
+
+func (m *MinIOReconciler) generateCredsSecret() *corev1.Secret {
+	return &corev1.Secret{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "v1",
+			Kind:       "Secret",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:        m.HarborCluster.Name + "-" + DefaultCredsSecret,
+			Namespace:   m.HarborCluster.Namespace,
+			Labels:      m.getLabels(),
+			Annotations: m.generateAnnotations(),
+		},
+		Type: corev1.SecretTypeOpaque,
+		Data: map[string][]byte{
+			"accesskey": []byte(CredsAccesskey),
+			"secretkey": []byte(CredsSecretkey)},
+	}
 }
