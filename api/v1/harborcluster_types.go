6df7dd3f91d116823e6b2a3d70bb9adaba0c7577
Xiaoyang Zhu <zhuxiaoyang1996@gmail.com>
2020-05-15 20:14:30 +0800

    fix resource definition
    Signed-off-by: Xiaoyang Zhu <zhuxiaoyang1996@gmail.com>

--- api/v1/harborcluster_types.go
+++ api/v1/harborcluster_types.go
@@ -250,7 +250,7 @@ type PostgresSQL struct {
 	Replicas         int                 `json:"replicas,omitempty"`
 	Version          string              `json:"version,omitempty"`
 	StorageClassName string              `json:"storageClassName,omitempty"`
-	Resources        corev1.ResourceList `json:"resources,omitempty"`
+	Resources        corev1.ResourceRequirements `json:"resources,omitempty"`
 
 	// External params following.
 	// The secret must contains "address:port","usernane" and "password".
@@ -306,7 +306,7 @@ type Sentinel struct {
 
 type RedisServer struct {
 	Replicas         int                 `json:"replicas,omitempty"`
-	Resources        corev1.ResourceList `json:"resources,omitempty"`
+	Resources        corev1.ResourceRequirements `json:"resources,omitempty"`
 	StorageClassName string              `json:"storageClassName,omitempty"`
 	// the size of storage used in redis.
 	Storage string `json:"storage,omitempty"`
