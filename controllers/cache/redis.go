9f3ad37293201608f401d519f1fb1d008e1221cc
Herman Zhu <zhuhuijunzhj@gmail.com>
2020-05-17 14:02:51 +0800

    Update redis.go

--- controllers/cache/redis.go
+++ controllers/cache/redis.go
@@ -1,8 +1,13 @@
 package cache
 
 import (
+	"github.com/go-logr/logr"
 	goharborv1 "github.com/goharbor/harbor-cluster-operator/api/v1"
+	"github.com/goharbor/harbor-cluster-operator/controllers/k8s"
 	"github.com/goharbor/harbor-cluster-operator/lcm"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/tools/record"
 )
 
 // RedisReconciler implement the Reconciler interface and lcm.Controller interface.
