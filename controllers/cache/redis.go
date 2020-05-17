706e21e5330a98ff4affbe7d828e03016f604157
Herman Zhu <zhuhuijunzhj@gmail.com>
2020-05-17 14:00:34 +0800

    Update redis.go

--- controllers/cache/redis.go
+++ controllers/cache/redis.go
@@ -18,37 +18,16 @@ type RedisReconciler struct {
 	Labels        map[string]string
 	Name          string
 	Namespace     string
+	CRStatus      *lcm.CRStatus
 }
 
 // Reconciler implements the reconcile logic of redis service
 func (redis *RedisReconciler) Reconcile() (*lcm.CRStatus, error) {
-	redis.Labels = redis.NewLabels()
-	redis.Name = redis.GetHarborClusterName()
-	redis.Namespace = redis.GetHarborClusterNamespace()
-
-	crStatus := &lcm.CRStatus{
-		Phase:      lcm.PendingPhase,
-		Properties: nil,
-	}
-
-	crStatus, err := redis.Provision(crStatus)
-	if err != nil {
-		return crStatus, err
-	}
 	return nil, nil
 }
 
-func (redis *RedisReconciler) Provision(crStatus *lcm.CRStatus) (*lcm.CRStatus, error) {
-	crStatus, err := redis.Deploy(crStatus)
-	if err != nil {
-		return crStatus, err
-	}
-
-	crStatus, err = redis.Readiness(crStatus)
-	if err != nil {
-		return crStatus, err
-	}
-	return crStatus, nil
+func (redis *RedisReconciler) Provision(spec *goharborv1.HarborCluster) (*lcm.CRStatus, error) {
+	panic("implement me")
 }
 
 func (redis *RedisReconciler) Delete() (*lcm.CRStatus, error) {
