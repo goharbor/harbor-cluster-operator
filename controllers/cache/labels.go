e66a0dbabc1bcf58d3c5d20de03f266cf2730d9a
Herman Zhu <zhuhuijunzhj@gmail.com>
2020-05-24 22:59:54 +0800

    Create labels.go

---
+++ controllers/cache/labels.go
@@ -0,0 +1,41 @@
+package cache
+
+const (
+	AppLabel = "harbor-cluster-operator"
+)
+
+// NewLabels returns new labels
+func (redis *RedisReconciler) NewLabels() map[string]string {
+	dynLabels := map[string]string{
+		AppLabel:                     redis.Name,
+		"app.kubernetes.io/name":     "cache",
+		"app.kubernetes.io/instance": redis.Namespace,
+		"service.owner":              "goharbor.io",
+		"owned.by":                   "harbor-cluster",
+	}
+
+	return MergeLabels(redis.Labels, dynLabels, redis.HarborCluster.Labels)
+}
+
+// MergeLabels merge new label to existing labels
+func MergeLabels(allLabels ...map[string]string) map[string]string {
+	res := map[string]string{}
+
+	for _, labels := range allLabels {
+		if labels != nil {
+			for k, v := range labels {
+				res[k] = v
+			}
+		}
+	}
+	return res
+}
+
+// generateLabels returns labels
+func generateLabels(component, role string) map[string]string {
+	return map[string]string{
+		"app":       AppLabel,
+		"component": component,
+		component:   role,
+	}
+}
