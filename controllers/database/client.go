abd2982ba92b7ea0bb521b919b56455456037a2b
Herman Zhu <zhuhuijunzhj@gmail.com>
2020-05-30 18:52:48 +0800

    Create client.go

---
+++ controllers/database/client.go
@@ -0,0 +1,17 @@
+package database
+
+import "fmt"
+
+type Connect struct {
+	Host     string
+	Port     string
+	Password string
+	Username string
+	Database string
+}
+
+// GenDatabaseUrl returns database connection url
+func (c *Connect) GenDatabaseUrl() string {
+	databaseURL := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", c.Username, c.Password, c.Host, c.Port, "postgres")
+	return databaseURL
+}
