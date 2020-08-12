4fedda3a64ed7ab6643a9b34d8f9688ae56e338b
wangcanfeng <wangcanfeng@corp.netease.com>
2020-08-12 18:32:50 +0800

    feat(api): print more info when using `kubectl get harborcluster`
    CLOSE #145
    
    Signed-off-by: wangcanfeng <wangcanfeng@corp.netease.com>

--- api/v1/harborcluster_types.go
+++ api/v1/harborcluster_types.go
@@ -412,10 +412,10 @@ type HarborClusterCondition struct {
 // +kubebuilder:object:root=true
 // +kubebuilder:printcolumn:name="Version",type=string,JSONPath=`.spec.version`,description="The semver Harbor version",priority=5
 // +kubebuilder:printcolumn:name="Public URL",type=string,JSONPath=`.spec.publicURL`,description="The public URL to the Harbor application",priority=0
-// +kubebuilder:printcolumn:name="HarborReady", type=string,JSONPath=`.status.conditions[?(@.type=="HarborReady")].status`,description="The current status of the new Harbor spec",priority=10
-// +kubebuilder:printcolumn:name="CacheReady", type=string,JSONPath=`.status.conditions[?(@.type=="CacheReady")].status`,description="The current status of the new Cache spec",priority=20
-// +kubebuilder:printcolumn:name="DatabaseReady", type=string,JSONPath=`.status.conditions[?(@.type=="DatabaseReady")].status`,description="The current status of the new Database spec",priority=20
-// +kubebuilder:printcolumn:name="StorageReady", type=string,JSONPath=`.status.conditions[?(@.type=="StorageReady")].status`,description="The current status of the new Storage spec",priority=20
+// +kubebuilder:printcolumn:name="Service Ready", type=string,JSONPath=`.status.conditions[?(@.type=="ServiceReady")].status`,description="The current status of the new Harbor spec",priority=10
+// +kubebuilder:printcolumn:name="Cache Ready", type=string,JSONPath=`.status.conditions[?(@.type=="CacheReady")].status`,description="The current status of the new Cache spec",priority=20
+// +kubebuilder:printcolumn:name="Database Ready", type=string,JSONPath=`.status.conditions[?(@.type=="DatabaseReady")].status`,description="The current status of the new Database spec",priority=20
+// +kubebuilder:printcolumn:name="Storage Ready", type=string,JSONPath=`.status.conditions[?(@.type=="StorageReady")].status`,description="The current status of the new Storage spec",priority=20
 
 // HarborCluster is the Schema for the harborclusters API
 type HarborCluster struct {
